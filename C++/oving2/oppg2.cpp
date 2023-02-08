c105d0560340342b21202ef72f79f9d8c6e69cfb
maerka <72936601+maerka@users.noreply.github.com>
2023-02-08 20:02:31 +0100

    c++ endre mappenavn

---
+++ C++/oving2/oppg2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+/*
+//oppg A)
+int inputIntegersUsingLoopAndPrintSum() {
+	cout << "Skriv inn antall tall som skal summeres. Skriv 0 hvis du ikke vil angi på forhånd antall ledd:" << endl;
+	int x;
+	return x = 1;
+}
+
+void main2() {
+	inputIntegersUsingLoopAndPrintSum();
+}*/
